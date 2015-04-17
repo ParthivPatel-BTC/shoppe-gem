586784596eaea4c943654e686b6f1d7b6fb5c3a8
parthivpatel <parthiv.patel@botreeconsulting.com>
2015-04-17 13:56:04 +0530

    Completed States module with CRUD operations

--- app/controllers/shoppe/states_controller.rb
+++ app/controllers/shoppe/states_controller.rb
@@ -1,7 +1,9 @@
-require_dependency "shoppe/application_controller"
-
 module Shoppe
   class StatesController < ApplicationController
+
+    before_filter { @active_nav = :states }
+    before_filter { params[:id] && @state = Shoppe::State.find(params[:id]) }
+
     def index
       @states = Shoppe::State.ordered
     end
@@ -9,5 +11,38 @@ module Shoppe
     def new
       @state = Shoppe::State.new
     end
+
+    def create
+      @state = Shoppe::State.new(safe_params)
+      if @state.save
+        redirect_to :states, :flash => {:notice => t('shoppe.states.create_notice')}
+      else
+        render :action => "new"
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @state.update(safe_params)
+        redirect_to [:edit, @state], :flash => {:notice => t('shoppe.states.update_notice') }
+      else
+        render :action => "edit"
+      end
+    end
+
+    def destroy
+      @state.destroy
+      redirect_to :states, :flash => {:notice => t('shoppe.states.destroy_notice')}
+    end
+
+
+    private
+
+    def safe_params
+      params[:state].permit(:name, :code2, :country_id)
+    end
+
   end
 end
--- app/models/shoppe/state.rb
+++ app/models/shoppe/state.rb
@@ -2,6 +2,12 @@ module Shoppe
   class State < ActiveRecord::Base
     belongs_to :country, :class_name => 'Shoppe::Country'
 
+    self.table_name = 'shoppe_states'
+
+    # Validations
+    validates :country_id, :presence => true
+    validates :name, :presence => true
+
     # All states ordered by their name asending
     scope :ordered, -> { order(:country_id => :asc) }
   end
